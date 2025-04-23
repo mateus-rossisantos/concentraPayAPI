8aef19607534996ec4aaa5242c6e7465d97bedea
naza21 <mateus.rossisantos@gmail.com>
2025-04-22 22:57:34 -0300

    primeiro commit

---
+++ src/main/java/com/cobranca/concentrapay/ConcentrapayApplication.java
@@ -0,0 +1,13 @@
+package com.cobranca.concentrapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcentrapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcentrapayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cobranca/concentrapay/ConcentrapayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobranca.concentrapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcentrapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
