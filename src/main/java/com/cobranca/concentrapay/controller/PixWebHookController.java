89bf9569a2c458a1faa0ee6b50569577f0fd9578
naza21 <mateus.rossisantos@gmail.com>
2025-07-09 23:13:21 -0300

    criação webhook

---
+++ src/main/java/com/cobranca/concentrapay/controller/PixWebHookController.java
@@ -0,0 +1,18 @@
+package com.cobranca.concentrapay.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/webhook")
+public class PixWebHookController {
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> handlePixWebhook() {
+        return ResponseEntity.ok("200");
+    }
+}
